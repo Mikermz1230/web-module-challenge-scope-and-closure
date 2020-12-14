9b1e5fa7898ab1abd38d3607d1c6903e2e272e92
Michael Ramirez <mikermz95@yahoo.com>
2020-12-14 17:19:11 -0600

    Answered a couple of questions

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  The first one is a closure. It is a function whithin a function. Giving the innerfunction all the assets of its parent.
   2. Which of the two uses a closure? How can you tell?
-  
+  Two functions.
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
 */
