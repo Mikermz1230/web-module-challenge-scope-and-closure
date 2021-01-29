b80ec231a2f43d9bccc9305452c55d3e45dabbdf
unknown <Mikermz95@yahoo.com>
2021-01-29 04:24:19 -0600

    finished mvp

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  counter one has closure
   2. Which of the two uses a closure? How can you tell?
-  
+  theres's a self contained {} in the cod
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
 */
